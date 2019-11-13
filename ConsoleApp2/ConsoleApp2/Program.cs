7a156ab4171a97540282037ae8738484b75ac5e0
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-11-13 16:46:32 +0100

    Array

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static public void Main(string[] args)
+    {
+        int[] tal1 = new int[5];
+
+
+        for (int v = 0; v < 5; v++)
+        {
+            tal1[v] = int.Parse(Console.ReadLine());
+
+        }
+        
+        for (int v = 4; v >= 0; v--)
+        {
+            Console.WriteLine(tal1[v]);
+        }
+
+
+    }
+}
